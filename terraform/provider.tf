f8c75c51b5e570c84d764be987a48f589fbbe278
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-08-24 06:45:07 -0600

    Update provider.tf
    Configured the Terraform AWS provider and required Terraform version for the project.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,15 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  required_Version = ">= 1.6.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
