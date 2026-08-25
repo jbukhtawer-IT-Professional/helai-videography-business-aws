f3165d0d18166712ed849d175ae3e43f74629fd9
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-08-25 07:02:37 -0600

    Update variables.tf
    Added configurable aws region, enivronment, project name, ec2 instance type variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,26 @@
 
+variable "aws_region" {
+  description = "AWS region where the infrastructue will be deployed"
+  type        = string
+  default     = "ca-central-1"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "development"
+}
+
+variable "helai_video" {
+  description = "Name of the project"
+  type        = string
+  default     = "helai-videography"
+}
+
+vairable "ec2_instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+
