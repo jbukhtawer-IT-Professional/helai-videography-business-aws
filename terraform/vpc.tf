94dcd56bcbbc0b7ebb2d9c9acab88bdfe3b219bc
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-08-26 21:19:32 -0600

    Create Public Subnet
    Created a public subnet inside the VPC for the web server

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,3 +6,14 @@ resource "aws_vpc" "main" {
     Name = "${var.project_name}-vpc"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-subnet
+  }
+}
