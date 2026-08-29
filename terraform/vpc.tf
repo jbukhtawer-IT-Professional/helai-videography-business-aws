8b2900cfd2da4c75a2e7120df3fb546a11070446
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-08-29 11:27:15 -0600

    Add a public route table
    Created a public route table and associated it with the public subnet through the internet gateway.

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -25,3 +25,21 @@ resource "aws_internet_gateway" "main" {
     Name = "${var.project_name}-igw"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
