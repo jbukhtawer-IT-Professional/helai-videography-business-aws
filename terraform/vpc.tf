dc3bc75e8a325810dee3ff51f36a0d112af71a51
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-08-30 10:44:07 -0600

    Create private subnet
    Created a private subnet in the VPC for private AWS resources such as the RDS database

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -43,3 +43,13 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "${var.aws_region}a"
+
+  tags = {
+    Name = "${var.project_name}-private-subnet"
+  }
+}
