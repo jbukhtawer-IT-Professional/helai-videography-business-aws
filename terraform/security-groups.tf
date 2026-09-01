124019951018ddca58f97d9a9a34622bcf4ba5a8
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-08-31 19:09:37 -0600

    Add Ec2 security group
    Created a security group for the EC2 web server allowing HTTP and HTTPS inbound traffic and outbound connectivity

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -1,1 +1,33 @@
+resource "aws_security_group" "ec2" {
+  name        = "${var.project_name}-ec2-sg"
+  description = "Security group for the EC2 web server"
+  vpc_id      = aws_vpc.main.id
 
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-ec2-sg"
+  }
+}
