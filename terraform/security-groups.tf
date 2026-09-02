bcc6c9b414377902bd6ec351a7c0b3c8908c1547
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-09-01 19:53:43 -0600

    Add RDS security group
    Created an RDS security group allowing MYSQL traffic only from EC2 security group

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -31,3 +31,31 @@ resource "aws_security_group" "ec2" {
     Name = "${var.project_name}-ec2-sg"
   }
 }
+
+
+
+resource "aws_security_group" "rds {
+  name        = "${var.project_name}-rds-sg"
+  description = "Security group for the RDS database"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow database traffic from EC2"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2.id]
+  }
+  
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-rds-sg"
+  }
+}
