e904c60822ee5d1834807bac265a033b6ae75b7e
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-09-02 19:31:59 -0600

    Add Amazon Linux AMI data source

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,1 +1,21 @@
 
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["a"x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
