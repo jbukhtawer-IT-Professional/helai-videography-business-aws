ba97e79be55fdb92944377c1baf522312dbb1e42
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-09-03 19:24:38 -0600

    Create EC2 web server

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -19,3 +19,21 @@ data "aws_ami" "amazon_linux" {
     values = ["hvm"]
   }
 }
+
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.ec2_instance_type
+
+
+  subnet_id = aws_subnet.public.id
+
+  vpc_security_group_ids = [
+    aws_security_group.ec2.id
+  ]
+
+  tags = {
+    Name = "${var.project_name}-web-server"
+  }
+}
