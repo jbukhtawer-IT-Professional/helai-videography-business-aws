62a55e760bd8b7e4336c40daa3d26c58d39483c8
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-09-06 11:06:14 -0600

    Add IAM role and SSM access for EC2
    Created an IAM role and instance profile for EC2 System Manager access and attached it to the web server

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,1 +1,40 @@
-  
+#An EC2 instance is allowed to assume/use this role/Created the role/Give this role the permissions needed for Systems Manager
+  resource "aws_iam_role" "ec2_ssm" {
+    name = "${var.project_name}-ec2-ssm-role"
+
+    assume_role_policy = jsonencode({
+      Version = "2012-10-17"
+
+      Statement = [
+        {
+          Effect = "Allow"
+
+          Principal = {
+            Service = "ec2.amazonaws.com"
+          }
+
+          Action = "sts:AssumeRole"
+        }
+      ]
+    })
+
+    tags = {
+      Name = "${var.project_name}-ec2-ssm-role"
+    }
+  }
+
+
+#This attaches AWS's AmazonSSMManagedInstanceCore managed policy to our role/Give this role the permissions needed for Systems Manager
+resource "aws_iam_role_policy_attachment" "ec2_ssm" {
+  role       = aws_iam_role.ec2_ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+
+
+#This is the bridge between EC2 and the IAM role.
+resource "aws_iam_instance_profile" "ec2_ssm" {
+  name = "${var.project_name}-ec2-ssm-profile"
+  role = aws_iam_role.ec2_ssm.name
+}
+
