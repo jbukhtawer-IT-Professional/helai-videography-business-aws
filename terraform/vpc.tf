e10368fe310abf36fdebd575253207c884c39c4a
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-09-09 20:05:59 -0600

    Add Second private subnet for RDS
    Added a second private subnet in another Availability Zone and associated it with the private route table for RDS networking

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -68,3 +68,18 @@ resource "aws_route_table_association" "private" {
 }
 
 
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "${var.aws_region}b"
+
+  tags = {
+    Name = "${var.project_name}-private-subnet-2"
+  }
+}
+
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private.id
+}
