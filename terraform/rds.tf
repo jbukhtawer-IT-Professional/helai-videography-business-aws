71d1a4c09b5d6a780d8dd38f36d863cdb02f57a1
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-09-11 20:09:25 -0600

    Create RDS DB subnet group
    Created an RDS DB subnet group using private subnets in two availability zones

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,1 +1,12 @@
+resource "aws_db_subnet_group" "rds" {
+  name = "${var.project_name}-rds-subnet-group"
 
+  subnet_ids = [
+    aws_subnet.private.id,
+    aws_subnet.private_2.id
+  ]
+
+  tags = {
+    Name = "${var.project_name}-rds-subnet-group"
+  }
+}
