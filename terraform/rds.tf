c7b5989de47becb65de2f055efc868ae63f6e763
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-09-15 19:03:00 -0600

    Create a private MySQL database
    Created a private MySQL RDS database using the RDS subnet group and security group

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -10,3 +10,32 @@ resource "aws_db_subnet_group" "rds" {
     Name = "${var.project_name}-rds-subnet-group"
   }
 }
+
+
+resource "aws_db_instance" "main" {
+  identifier  = "${var.project_name}-database"
+
+  engine          = "mysql"
+  engine_version  = "8.4"
+
+  instance_class  = "db.t3.micro"
+
+  allocated_storage = 20
+  storage_type      = "gp3"
+
+  db_name  = "helai"
+  username = "admin"
+  password = var.db_password
+
+  db_subnet_group_name  = aws_db_subnet_group.rds.name
+  vpc_security_group_ids  = [aws_security_group.rds.id]
+
+  publicly_accessible = false
+
+  skip_final_snapshot  = true
+
+  tags  = {
+    Name = "${var.project_name}-database"
+  }
+}
+  
