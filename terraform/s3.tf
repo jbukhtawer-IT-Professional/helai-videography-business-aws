c46ffc82ec0d82b6b8b114ac8868e567ca662938
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-09-17 17:43:21 -0600

    Add secure S3 application storage
    Created an S3 application storage bucket and blocked public access

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,1 +1,19 @@
 
+#Create an S3 bucket and call it app inside Terraform.
+resource "aws_s3_bucket" "app" {
+  bucket = "{var.project_name}-app-storage"
+#Terraform builds the bucket name using your project name.
+
+  tags = {
+    Name  = "${var.project_name}-app-storage"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "app" {
+  bucket = aws_s3_bucket.app.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restric_public_buckets  = true
+}
