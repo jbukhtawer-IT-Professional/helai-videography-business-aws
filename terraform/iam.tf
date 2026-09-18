d03e3181125ed31b319ec395a6b32a083c4dba23
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-09-18 09:49:56 -0600

    Add S3 permissions to Ec2 role
    Allow the Ec2 instance to securely upload, download, delete and list objects in the application S3 bucket

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -38,3 +38,38 @@ resource "aws_iam_instance_profile" "ec2_ssm" {
   role = aws_iam_role.ec2_ssm.name
 }
 
+
+
+resource ="aws_iam_role_policy" "ec2_s3" {
+  namw = "${var.project_name}-ec2-s3-policy"
+  role = aws_iam_role.ec2.ssm.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+
+#the object permissions apply to objects inside your specific bucket.
+        Resource = "${aws_s3_bucket.app.arn}/*"  
+      },
+      {
+        Effect = "Allow"
+
+        Action = [
+          "s3:ListBucket"
+        ]
+
+#is used for the bucket-level ListBucket permission.
+        Resource = aws_s3_bucket.app.arn
+      }
+    ]
+  })
+}
