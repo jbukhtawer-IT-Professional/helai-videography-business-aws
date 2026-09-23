1ceae4da7a9ccee8f741128e0b0e088f663a765e
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-09-22 18:27:32 -0600

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,15 +1,14 @@
-
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
-  required_Version = ">= 1.6.0"
+
+  required_version = ">= 1.6.0"
 }
 
 provider "aws" {
   region = var.aws_region
 }
-
