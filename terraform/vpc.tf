c8f42f036d5adfab83a34c56d1772327a7e94d01
jbukhtawer-IT-Professional <jbukhtawer@gmail.com>
2026-09-23 18:25:45 -0600

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "${var.project_name}-public-subnet
+    Name = "${var.project_name}-public-subnet"
   }
 }
 
